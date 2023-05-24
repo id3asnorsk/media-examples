a08f5c587f76ebf72d9cab5ce15a64d4d40881a4
Adrian Roe <roe.adrian@gmail.com>
2023-05-24 11:06:46 +0100

    Move repo to norskvideo

--- examples/src/00_hello_norsk.ts
+++
@@ -1,7 +0,0 @@
-import { Norsk, selectAV } from "@id3asnorsk/norsk-sdk"
-
-export async function main() {
-  const norsk = await Norsk.connect({});  
-  console.log(`Hello from norsk version: ${norsk.version.label}`); 
-  norsk.close(); 
-}
